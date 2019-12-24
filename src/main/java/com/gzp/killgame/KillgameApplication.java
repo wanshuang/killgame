2aa61585d7e278737da65f6572d85ee893dc7fa3
wanshuang <wanshuang@gozap.com>
2019-12-24 18:23:24 +0800

    Initial commit

---
+++ src/main/java/com/gzp/killgame/KillgameApplication.java
@@ -0,0 +1,13 @@
+package com.gzp.killgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KillgameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KillgameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gzp/killgame/KillgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gzp.killgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KillgameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
